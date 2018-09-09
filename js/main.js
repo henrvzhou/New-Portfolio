93bdaec719f3fc75790c938a23dffdc608dbb06e
henryz <hez020@ucsd.edu>
2018-09-08 17:19:02 -0700

    projects section added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 //javascript
+
+const myNav = document.getElementById('myNavbar');
+let body = document.body;
+let html = document.documentElement;
+
+window.onscroll = () => {
+  if (body.scrollTop >= 100 || html.scrollTop >= 100) {
+    myNav.classList.add('navScroll');
+  } else {
+    myNav.classList.remove('navScroll');
+  }
+}
+
